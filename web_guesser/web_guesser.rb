3d51f032b2aeda4d6e3dfdb2c36b0214ea4453a4
LeanKhan <eslean20@gmail.com>
2020-01-19 21:24:01 +0100

    feat: add erb and html

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -3,10 +3,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-    
+
     srand 1234
     secret_number = rand(101)
     srand 1234
 
-    "The SECRET NUMBER is #{secret_number}"
+    erb :index, :locals => {:number => secret_number}
 end
