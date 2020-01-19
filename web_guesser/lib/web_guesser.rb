ac63d23791074934130528e6cda38ac154f68b6c
LeanKhan <eslean20@gmail.com>
2020-01-19 20:59:25 +0100

    feat: display random number, add sinatra reloader

--- web_guesser/lib/web_guesser.rb
+++ web_guesser/lib/web_guesser.rb
@@ -1,6 +1,12 @@
 # web_guesser app thingy
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
-    "Hello World!" + req
+    
+    srand 1234
+    secret_number = rand(101)
+    srand 1234
+
+    "The SECRET NUMBER is #{secret_number}"
 end
