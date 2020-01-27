921d202594f49c7a92817f840d8d755da291d3f4
LeanKhan <eslean20@gmail.com>
2020-01-27 20:17:54 +0100

    feat: add Gemfile

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -5,8 +5,21 @@ require 'sinatra/reloader'
 get '/' do
 
     srand 1234
-    secret_number = rand(101)
+    number = rand(101)
     srand 1234
 
-    erb :index, :locals => {:number => secret_number}
+    guess = params["guess"] ? params["guess"].to_i : nil
+
+    if(guess)
+        if(guess > number)
+            message = "Too High!"
+        elsif(guess < number)
+            message = "Too Low!"
+        else
+            message = "Just Right!"
+        end
+    end
+
+    erb :index, :locals => {:number => number, :message => message, :guess => guess}
+
 end
